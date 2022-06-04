0743770ea5b3e421ca1f0127843dd1bf94fea8c4
NeleB54Gold <51356760+NeleB54Gold@users.noreply.github.com>
2022-06-04 20:04:22 +0200

    Release of NeleBot X 1.1.0!
    NeleBot X Framework updated to Telegram Bot API 6.0!
    Added methods, variables and other bug-fixes!

--- configs.php
+++ configs.php
@@ -11,13 +11,13 @@ $configs = [
 		# 3. Default parse mode					(Optional)	[Text: html/markdown/markdownv2]
 		'parse_mode'				=> 'html',
 		# 4. Default web page preview			(Optional)	[Bool]
-		'disable_web_page_preview'	=> 1,
+		'disable_web_page_preview'	=> true,
 		# 5. Send without reply					(Optional)	[Bool]
-		'allow_sending_w/out_reply'	=> 1,
+		'allow_sending_w/out_reply'	=> true,
 		# 6. Send without notification sound	(Optional)	[Bool]
-		'disable_notification'		=> 0,
+		'disable_notification'		=> false,
 		# 7. Protect contents					(Optional)	[Bool]
-		'protect_content'			=> 0,
+		'protect_content'			=> false,
 		# 8. Commands alias						(Optional)	[Array of Text(Only 1 character)]
 		'commands_alias'			=> ['/', '!'],
 	
@@ -27,32 +27,32 @@ $configs = [
 		# 2. Choose the errors to log			(Optional)	[Array of Number => bool]
 		'log_types'					=> [
 			# https://www.php.net/manual/en/errorfunc.constants.php
-			E_ERROR						=> 1,
-			E_PARSE						=> 1,
-			E_CORE_ERROR				=> 1,
-			E_COMPILE_ERROR				=> 1,
-			E_USER_WARNING				=> 0,
-			E_USER_NOTICE				=> 0,
-			E_RECOVERABLE_ERROR			=> 0
+			E_ERROR						=> true,
+			E_PARSE						=> true,
+			E_CORE_ERROR				=> true,
+			E_COMPILE_ERROR				=> true,
+			E_USER_WARNING				=> false,
+			E_USER_NOTICE				=> false,
+			E_RECOVERABLE_ERROR			=> false
 		],
 	
 	# 3. Databases
 		# 1. Redis								(Optional)
 		'redis'						=> [
 			# 1. Turn on/off Redis					(Optional)	[Bool]
-			'status'					=> 1,
+			'status'					=> false,
 			# 2. Select the Redis database			(Optional)	[Number]
 			'database'					=> 1,
 			# 3. Select Redis Server host			(Required)	[Redis Server IP]
 			'host'						=> 'localhost',
 			# 4. Select Redis Server port			(Optional)	[Number]
 			'port'						=> 6379,
-			# 5. Password for your Redis Server		(Optional)	[Text/0]
-			'password'					=> 0,
+			# 5. Password for your Redis Server		(Optional)	[Text/false]
+			'password'					=> false,
 			# 6. Anti Flood System					(Optional)
 			'antiflood'					=> [
 				# 1. Turn on/off the AntiFlood			(Optional)	[Bool]
-				'status'					=> 1,
+				'status'					=> true,
 				# 2. Set the messages to be sent		(Required)	[Number]
 				'messages'					=> 5,
 				# 3. Set the seconds to be sent			(Required)	[Number]
@@ -66,7 +66,7 @@ $configs = [
 		# 2. MySQL/SQLite/PostgreSQL			(Optional)
 		'database' 					=> [
 			# 1. Turn on/off the database 			(Optional)	[Bool]
-			'status'					=> 1,
+			'status'					=> false,
 			# 2. Choose your database				(Required)	[Text: sqlite, mysql, pgsql]
 			'type'						=> '',
 			# 3. Database name (file for SQLite)	(Required)	[Text]
@@ -86,17 +86,17 @@ $configs = [
 		],
 		# 2. Plugins list						(Optional)	[Array of file name(text) => status(bool)]
 		'plugins'					=> [
-			'management.php'			=> 1,
-			'commands.php'				=> 1
+			'management.php'			=> true,
+			'commands.php'				=> true
 		],
 		# 2. Default requests response			(Optional)	[Bool]
-		'response'					=> 0,
-		# 3. Default requests method			(Optional)	[Bool: 1(POST)/0(GET)]
-		'post'						=> 1,
+		'response'					=> false,
+		# 3. Default requests method			(Optional)	[Bool: true(POST)/false(GET)]
+		'post'						=> true,
 		# 4. Default requests timeout seconds	(Optional)	[Number]
 		'timeout'					=> 2,
 		# 5. NeleBotX Framework version
-		'version'					=> '1.0.0'
+		'version'					=> '1.1.0'
 ];
 
 ?>
